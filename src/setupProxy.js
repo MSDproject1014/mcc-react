6885b0c23f7195e8a2ef7173bec9690ddc6800a7
user <user@this.com>
2022-11-01 16:46:33 -0400

    ''

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,6 +5,6 @@ module.exports = function(app) {
 //    app.use(proxy('/api', { target: 'http://localhost:8080/' }));
 //    app.use(proxy('/account', { target: 'http://localhost:8081/' })); 
 	
-	app.use(proxy('/api', { target: 'http://10.111.113.190:8080/' }));
-	app.use(proxy('/account', { target: 'http://10.111.90.40:8081/' }));
+	app.use(proxy('/api', { target: 'http://10.100.231.169:8080/' }));
+	app.use(proxy('/account', { target: 'http://10.103.85.94:8081/' }));
 };
