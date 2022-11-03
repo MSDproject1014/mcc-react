ab9ed0515989628ab0af78296d6927f88c584294
skhamisi <skhamisi@outlook.com>
2022-11-03 14:41:37 -0400

    Updated proxy IPs

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 	
-	app.use(proxy('/api', { target: 'http://10.101.15.104:8080/' }));
-	app.use(proxy('/account', { target: 'http://10.99.244.48:8081/' }));
+    app.use(proxy('/api', { target: 'http://10.101.15.104:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.99.244.48:8081/' }));
 };
 
 test2
