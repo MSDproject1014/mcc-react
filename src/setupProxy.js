8bd6185ec71a95e6bfbd3e3d29d6e9f1351fe929
skhamisi <skhamisi@outlook.com>
2022-11-03 14:43:12 -0400

    Updated proxy IPs

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,5 +5,3 @@ module.exports = function(app) {
     app.use(proxy('/api', { target: 'http://10.101.15.104:8080/' }));
     app.use(proxy('/account', { target: 'http://10.99.244.48:8081/' }));
 };
-
-test3
