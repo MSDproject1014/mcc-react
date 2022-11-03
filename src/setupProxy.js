ef12e7b9e9c0ab18c8f01886dda6d7659cefd73a
user <user@this.com>
2022-11-02 20:24:40 -0400

    ''

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,6 +5,6 @@ module.exports = function(app) {
 //    app.use(proxy('/api', { target: 'http://localhost:8080/' }));
 //    app.use(proxy('/account', { target: 'http://localhost:8081/' })); 
 	
-	app.use(proxy('/api', { target: 'http://10.100.135.44:8080/' }));
-	app.use(proxy('/account', { target: 'http://10.98.22.188:8081/' }));
+	app.use(proxy('/api', { target: 'http://10.101.15.104:8080/' }));
+	app.use(proxy('/account', { target: 'http://10.99.244.48:8081/' }));
 };
