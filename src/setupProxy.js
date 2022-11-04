0d262725276660f9a4e7494ae1bcc3146ff9d787
carloschavira333 <carlos.chavira333@gmail.com>
2022-11-04 12:56:33 -0400

    setup.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api', { target: 'http://10.104.159.142:8080/' }));
+
     app.use(proxy('/api', { target: 'http://10.98.122.59:8080/' }));
     app.use(proxy('/account', { target: 'http://10.98.228.43:8081/' }));
