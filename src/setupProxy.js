516ca341a9cd180288020806a377fa97d066b2cd
carloschavira333 <carlos.chavira333@gmail.com>
2022-11-03 20:24:22 -0400

    Updated proxy IPs

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,5 +2,5 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(proxy('/api', { target: 'http://10.104.159.142:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.108.56.162:8081/' }));
-};
+    app.use(proxy('/api', { target: 'http://10.108.231.228:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.98.62.192:8081/' }));
